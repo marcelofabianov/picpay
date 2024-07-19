d68e3415bcb545d3a38729807b3aa7b2a440a272
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-19 18:12:42 -0300

    feat: definir tipos inicias do domain

--- cmd/main.go
+++ cmd/main.go
@@ -20,5 +20,5 @@ func main() {
 
 	defer logger.Close()
 
-	logger.Info("logger created")
+	logger.Info("ONLINE")
 }
---
+++ internal/domain/type.go
@@ -0,0 +1,13 @@
+package domain
+
+import "time"
+
+type ID string
+type Email string
+type Password string
+type DocumentRegistry string
+type Amount float64
+type CreatedAt time.Time
+type UpdatedAt time.Time
+type DeletedAt time.Time
+type Version int
