be6fb86b449db8d30172a97e430809c66a07a132
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-19 18:21:43 -0300

    chore: remover type de users

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -2,18 +2,12 @@ package domain
 
 type UserType string
 
-const (
-	Common   UserType = "user"
-	Merchant UserType = "merchant"
-)
-
 type User struct {
 	ID               ID               `json:"id" validate:"required,uuid"`
 	Name             string           `json:"name" validate:"required,min=3,max=255,string"`
 	Email            Email            `json:"email" validate:"required,email,max=150,string"`
 	Password         Password         `json:"password" validate:"required,min=8,max=255,string"`
 	DocumentRegistry DocumentRegistry `json:"document_registry" validate:"required,min=11,max=14,string"`
-	Type             UserType         `json:"type" validate:"required,oneof=common merchant"`
 	Enabled          bool             `json:"enabled" validate:"required,bool"`
 	CreatedAt        CreatedAt        `json:"created_at"`
 	UpdatedAt        UpdatedAt        `json:"updated_at"`
