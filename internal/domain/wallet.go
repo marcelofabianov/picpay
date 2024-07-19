841a6cec2dd506fc9b1cc74bf226ccab8ba05ddc
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-19 18:22:13 -0300

    feat: adicionar type entidade wallet

--- internal/domain/wallet.go
+++ internal/domain/wallet.go
@@ -1,9 +1,17 @@
 package domain
 
+type WalletType string
+
+const (
+	WalletTypeCommon   WalletType = "user"
+	WalletTypeMerchant WalletType = "merchant"
+)
+
 type Wallet struct {
 	ID        ID         `json:"id" validate:"required,uuid"`
 	UserID    ID         `json:"user_id" validate:"required,uuid"`
 	Amount    Amount     `json:"amount" validate:"required,numeric"`
+	Type      WalletType `json:"type" validate:"required,oneof=user merchant"`
 	Enabled   bool       `json:"enabled" validate:"required,bool"`
 	CreatedAt CreatedAt  `json:"created_at"`
 	UpdatedAt UpdatedAt  `json:"updated_at"`
