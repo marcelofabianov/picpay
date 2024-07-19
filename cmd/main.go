87e40dad2fd798a9b3481c5a8ce8e06908a9010e
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-19 18:35:36 -0300

    feat: criar conexao com postgres no main

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/marcelofabianov/picpay/config"
+	"github.com/marcelofabianov/picpay/pkg/postgres"
 	"github.com/marcelofabianov/picpay/pkg/zap"
 )
 
@@ -20,5 +22,19 @@ func main() {
 
 	defer logger.Close()
 
-	logger.Info("ONLINE")
+	db, err := postgres.Connect(context.Background(), cfg.Db)
+	if err != nil {
+		logger.Fatal("error connecting to database")
+	}
+	defer func() {
+		if err := db.Close(context.Background()); err != nil {
+			logger.Fatal("error closing database connection")
+		}
+	}()
+
+	if err := db.Ping(context.Background()); err != nil {
+		logger.Fatal("error pinging database")
+	}
+
+	logger.Info("OK")
 }
