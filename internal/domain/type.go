02b1a0d1fe52a91f56312ca7803b9373b2f9b365
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-22 06:09:21 -0300

    feat: atualizar types de domain para acrescentar inicializador de types

--- internal/domain/type.go
+++ internal/domain/type.go
@@ -1,6 +1,10 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type ID string
 type Email string
@@ -11,3 +15,19 @@ type CreatedAt time.Time
 type UpdatedAt time.Time
 type DeletedAt time.Time
 type Version int
+
+func NewID() ID {
+	return ID(uuid.New().String())
+}
+
+func CreatedAtNow() CreatedAt {
+	return CreatedAt(time.Now())
+}
+
+func UpdatedAtNow() UpdatedAt {
+	return UpdatedAt(time.Now())
+}
+
+func InitVersion() Version {
+	return Version(1)
+}
