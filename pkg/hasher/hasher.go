ee7faf2677839e5fd71bf326b338d2491a0c8ade
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-22 07:21:00 -0300

    refactor: ajustar pkg hasher para cumprir interface PasswordHashser

--- pkg/hasher/hasher.go
+++ pkg/hasher/hasher.go
@@ -2,65 +2,95 @@ package hasher
 
 import (
 	"crypto/rand"
+	"crypto/subtle"
 	"encoding/base64"
 	"fmt"
+	"strings"
 
 	"golang.org/x/crypto/argon2"
 )
 
-type Argon2Hasher struct{}
+const (
+	saltLength = 16
+	time       = 1
+	memory     = 64 * 1024
+	threads    = 4
+	keyLength  = 32
+)
 
-func NewArgon2Hasher() *Argon2Hasher {
-	return &Argon2Hasher{}
+type Config struct {
+	Time    uint32
+	Memory  uint32
+	Threads uint8
+	KeyLen  uint32
 }
 
-func (h *Argon2Hasher) Hash(password string) (string, error) {
-	salt := make([]byte, 16)
+var defaultConfig = Config{
+	Time:    time,
+	Memory:  memory,
+	Threads: threads,
+	KeyLen:  keyLength,
+}
+
+type Hasher struct {
+	Config Config
+}
+
+func NewHasher() *Hasher {
+	config := Config{
+		Time:    time,
+		Memory:  memory,
+		Threads: threads,
+		KeyLen:  keyLength,
+	}
+
+	return &Hasher{Config: config}
+}
+
+func generateSalt() ([]byte, error) {
+	salt := make([]byte, saltLength)
 	_, err := rand.Read(salt)
+	if err != nil {
+		return nil, err
+	}
+	return salt, nil
+}
+
+func (h *Hasher) Hash(data string) (string, error) {
+	salt, err := generateSalt()
 	if err != nil {
 		return "", err
 	}
 
-	hash := argon2.IDKey([]byte(password), salt, 1, 64*1024, 4, 32)
+	hash := argon2.IDKey([]byte(data), salt, defaultConfig.Time, defaultConfig.Memory, defaultConfig.Threads, defaultConfig.KeyLen)
 
 	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
 	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
 
-	encodedHash := fmt.Sprintf("$argon2id$v=19$m=65536,t=1,p=4$%s$%s", b64Salt, b64Hash)
-
-	return encodedHash, nil
+	return fmt.Sprintf("%s$%s$%s", "argon2id", b64Salt, b64Hash), nil
 }
 
-func (h *Argon2Hasher) Compare(hashedPassword string, password string) error {
-	var salt, hash []byte
-	var err error
-
-	parts := make([][]byte, 3)
-
-	for i, part := range parts {
-		parts[i] = []byte(part)
+func (h *Hasher) Compare(data, encodedHash string) (bool, error) {
+	parts := strings.Split(encodedHash, "$")
+	if len(parts) != 3 {
+		return false, fmt.Errorf("invalid hash format")
 	}
 
-	_, err = fmt.Sscanf(hashedPassword, "$argon2id$v=19$m=65536,t=1,p=4$%s$%s", &parts[0], &parts[1])
+	salt, err := base64.RawStdEncoding.DecodeString(parts[1])
 	if err != nil {
-		return err
+		return false, err
 	}
 
-	salt, err = base64.RawStdEncoding.DecodeString(string(parts[0]))
+	hash, err := base64.RawStdEncoding.DecodeString(parts[2])
 	if err != nil {
-		return err
+		return false, err
 	}
 
-	hash, err = base64.RawStdEncoding.DecodeString(string(parts[1]))
-	if err != nil {
-		return err
+	newHash := argon2.IDKey([]byte(data), salt, defaultConfig.Time, defaultConfig.Memory, defaultConfig.Threads, uint32(len(hash)))
+
+	if subtle.ConstantTimeCompare(hash, newHash) == 1 {
+		return true, nil
 	}
 
-	compareHash := argon2.IDKey([]byte(password), salt, 1, 64*1024, 4, 32)
-
-	if string(compareHash) != string(hash) {
-		return fmt.Errorf("password does not match hash")
-	}
-
-	return nil
+	return false, nil
 }
